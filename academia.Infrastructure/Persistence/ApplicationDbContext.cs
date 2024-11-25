b5a559a47acd669bbe504213c96474e2759879e9
Daniel Bmend <danielbmendprofissional@gmail.com>
2024-11-25 19:47:42 -0300

    Add project files.

---
+++ academia.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using academia.Domain.Entidades;
+using academia.Infrastructure.Persistence.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace academia.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuario { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ academia.Infrastructure/Persistence/Mappings/UsuarioMap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using academia.Domain.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace academia.Infrastructure.Persistence.Mappings
+{
+    internal class UsuarioMap : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.ToTable("Usuario");
+
+            builder.HasKey(u => u.Id); 
+            builder.Property(u => u.Id).ValueGeneratedOnAdd();
+
+            builder.Property(c => c.Nome)
+                .IsRequired();
+
+            builder.Property(c => c.Email)
+                .IsRequired();
+
+            builder.Property(c => c.Senha)
+                .IsRequired();
+
+        }
+    }
+}
