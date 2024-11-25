a37a1012d59ce9c361f9e80d8b4b0ddea0e5766b
Daniel Bmend <danielbmendprofissional@gmail.com>
2024-11-25 20:04:04 -0300

    Configurando ambientes

--- academia.Infrastructure/Persistence/ApplicationDbContext.cs
+++ academia.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using academia.Domain.Entidades;
+﻿using academia.Domain.Entidades;
 using academia.Infrastructure.Persistence.Mappings;
 using Microsoft.EntityFrameworkCore;
 
