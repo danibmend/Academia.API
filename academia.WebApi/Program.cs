0f88b4613948089f429fb2ffc80c55c4435ec827
Daniel Bmend <danielbmendprofissional@gmail.com>
2024-11-30 14:04:15 -0300

    Finalizando

--- academia.WebApi/Program.cs
+++ academia.WebApi/Program.cs
@@ -1,21 +1,24 @@
+using System.Text;
 using academia.Application.Mappings;
 using academia.Infrastructure;
 using academia.Infrastructure.Persistence;
 using academia.WebApi.Controllers.Base;
 using academia.WebApi.Extensions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.Configure<ApplicationDbContext>(x => x.Database.Migrate());
 
@@ -29,16 +32,12 @@ builder.Services.LoadValidators();
 var app = builder.Build();
 
 app.UseSwagger();
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwaggerUI();
+app.UseSwaggerUI();
 
-    app.UseCors(options => options
-    .AllowAnyOrigin()
-    .AllowAnyMethod()
-    .AllowAnyHeader());
-}
+app.UseCors(options => options
+.AllowAnyOrigin()
+.AllowAnyMethod()
+.AllowAnyHeader());
 
 app.UseMiddleware(typeof(ErrorHandlerMiddleware));
 
